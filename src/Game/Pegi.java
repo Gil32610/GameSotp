7373a861b60d4bdc82b2a07d6b94017a71d30c3a
GIL32610 <gil.00000032610@unicap.br>
2022-10-20 14:40:10 -0300

    ENUM CLASS

---
+++ src/Game/Pegi.java
@@ -0,0 +1,10 @@
+package Game;
+
+public enum Pegi {
+    PEGI3,
+    PEGI7,
+    PEGI12,
+    PEGI16,
+    PEGI18;
+
+}
--- src/Person/User.java
+++ src/Person/User.java
@@ -1,14 +1,15 @@
 package Person;
+
 import LinkedList.*;
 import Game.*;
-public class User  extends Person{
+
+public class User extends Person {
     private String email;
     private String username;
     private Integer age;
     private SinglyLinkedList<Game> gameList;
-    
 
-    public User(String cpf, String fullname, String nationality, String email, String username, Integer age){
+    public User(String cpf, String fullname, String nationality, String email, String username, Integer age) {
         super(cpf, fullname, nationality);
         this.email = email;
         this.username = username;
@@ -17,9 +18,8 @@ public class User  extends Person{
 
     }
 
-    
+    public void addGame(Game g) {
+        this.gameList.addLast(g);
+    }
 
-
-
-    
 }
