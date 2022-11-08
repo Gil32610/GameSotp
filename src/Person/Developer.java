1ef4393f924a8633dbb49e2c8bd468a57cb7dbd4
GIL32610 <gil.00000032610@unicap.br>
2022-11-08 14:30:13 -0300

    Developer methods can now send games to publisher

--- src/Person/Developer.java
+++ src/Person/Developer.java
@@ -5,7 +5,7 @@ import LinkedList.*;
 import Studio.Publisher;
 import Game.*;
 
-public class Developer extends Person  {
+public class Developer extends Person {
 
     private String username;
     private String password;
@@ -17,16 +17,54 @@ public class Developer extends Person  {
         this.password = password;
     }
 
-    public void sendGamePublisher(Publisher p){
-        String title,genre; 
-        AgeRating rating;
+    public void sendGamePublisher(Publisher p) {
+        Scanner s = new Scanner(System.in);
+        String gameTitle, genre;
+        AgeRating ageRating = AgeRating.L;
         Integer releaseDate;
         double price;
-        Scanner s = new Scanner(System.in);
-        
-        
-        
+        Game g;
+        System.out.println("Informe ");
+        gameTitle = s.nextLine();
+        System.out.println("Informe ");
+        genre = s.nextLine();
+        age();
+        ageRating = rating(Integer.parseInt(s.nextLine()));
+        System.out.println("Informe ");
+        releaseDate = Integer.parseInt(s.nextLine());
+        System.out.println("Informe ");
+        price = Double.parseDouble(s.nextLine());
+        g = new Game(gameTitle, ageRating, genre, releaseDate, price);
+        p.addGame(g);
+    }
 
+    private AgeRating rating(int i) {
+
+        switch (i) {
+            case 1:
+                return AgeRating.L;
+            case 2:
+                return AgeRating.M10;
+            case 3:
+                return AgeRating.M12;
+
+            case 4:
+                return AgeRating.M14;
+            case 5:
+                return AgeRating.M16;
+            case 6:
+                return AgeRating.M18;
+            default:
+                return null;
+
+        }
+    }
+
+    private void age() {
+        System.out.println("1- classificação livre\n" + "2- Não recomendado para menores de 10 anos\n"
+                + "3 - Não recomendado para menores de 12 anos\n" + "4 - Não recomendado para menores de 14 anos\n"
+                + "5 - Não recomendado para menores de 16 anos\n" + "6 - Não recomendado para menores de 18 anos");
 
     }
+
 }
--- src/Store/GameSotp.java
+++ src/Store/GameSotp.java
@@ -10,7 +10,7 @@ public class GameSotp {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String fullname, email, nationality, cpf, username;
-        Integer age; 
+        Integer age;
         Game g;
         User user;
         AgeRating aRating;
@@ -19,59 +19,27 @@ public class GameSotp {
         do {
             options();
             op = Integer.parseInt(s.nextLine());
-            switch(op){
+            switch (op) {
                 case 1:
-                System.out.println("Informe o nome completo");
-                break;
+                    System.out.println("Informe o nome completo");
+                    break;
                 case 2:
-                break;
+                    break;
                 case 3:
-                break;
+                    break;
                 case 4:
-                break;
+                    break;
                 case 5:
-                break;
+                    break;
                 case 6:
-                break;
+                    break;
             }
 
         } while (op != 0);
-       
-        
-    }
 
-    
+    }
 
     public static void options() {
         System.out.println("1- criar nova conta\n" + "2- ver catálogo\n" + " ");
     }
-
-    public static void aRating() {
-        System.out.println("1- classificação livre\n" + "2- Não recomendado para menores de 10 anos\n"
-                + "3 - Não recomendado para menores de 12 anos\n" + "4 - Não recomendado para menores de 14 anos\n"
-                + "5 - Não recomendado para menores de 16 anos\n" + "6 - Não recomendado para menores de 18 anos");
-    }
-    public static AgeRating rating(int i){
-       
-        switch (i) {
-            case 1:
-                return AgeRating.L;
-            case 2:
-                return AgeRating.M10;
-            case 3:
-                return AgeRating.M12;
-
-            case 4:
-                return AgeRating.M14;
-            case 5:
-                return AgeRating.M16;
-            case 6:
-                return AgeRating.M18;
-            default:
-                return null;
-
-        }
-    }
-    
-
 }
